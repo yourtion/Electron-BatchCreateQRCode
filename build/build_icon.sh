7cc38ab21d1ae5a39b6e73874bba7770af698555
YourtionGuo <yourtion@gmail.com>
2018-07-20 17:23:55 +0800

    feat: add icon

---
+++ build/build_icon.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+convert -resize x1024 icon.png ./icons/1024x1024.png
+convert -resize x512 icon.png ./icons/512x512.png
+convert -resize x256 icon.png ./icons/256x256.png
+convert -resize x128 icon.png ./icons/128x128.png
+convert -resize x96 icon.png ./icons/96x96.png
+convert -resize x64 icon.png ./icons/64x64.png
+convert -resize x48 icon.png ./icons/48x48.png
+convert -resize x32 icon.png ./icons/32x32.png
+convert -resize x24 icon.png ./icons/24x24.png
+convert -resize x16 icon.png ./icons/16x16.png
