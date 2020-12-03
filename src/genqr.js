3e06410f1ab25008a9c47c6f5e9ad9119629e67c
YourtionGuo <yourtion@gmail.com>
2020-12-03 18:22:55 +0800

    0.8.0

--- src/genqr.js
+++ src/genqr.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const qr = require('qrcode');
-const xlsx = require('node-xlsx').default;
+const fs = require("fs");
+const path = require("path");
+const qr = require("qrcode");
+const xlsx = require("node-xlsx").default;
 
-const ipc = require('electron').ipcMain;
-const { dialog, shell } = require('electron');
+const ipc = require("electron").ipcMain;
+const { dialog, shell } = require("electron");
 
 let isOpen = false;
 let savePath;
@@ -13,18 +13,18 @@ let sourcePath;
 function leftPad(n, c) {
   let res = String(n);
   while (res.length < c) {
-    res = '0' + res;
+    res = "0" + res;
   }
   return res;
 }
 
 function isUrl(str) {
-  return String(str).indexOf('http') === 0;
+  return String(str).indexOf("http") === 0;
 }
 
 function getPrefix(source) {
-  const prefixArr = path.basename(source).split('_');
-  if(prefixArr.length > 1) {
+  const prefixArr = path.basename(source).split("_");
+  if (prefixArr.length > 1) {
     return prefixArr[0];
   }
   return;
@@ -33,7 +33,7 @@ function getPrefix(source) {
 function genQR(path, text, opts = {}) {
   return new Promise((resove, reject) => {
     qr.toFile(path, text, opts, (err) => {
-      if(err) reject(err);
+      if (err) reject(err);
       resove(path);
     });
   });
@@ -42,7 +42,7 @@ function genQR(path, text, opts = {}) {
 function readExcel(file) {
   return new Promise((resove, reject) => {
     fs.readFile(file, (err, buffer) => {
-      if(err) reject(err);
+      if (err) reject(err);
       const workSheets = xlsx.parse(buffer);
       resove(workSheets);
     });
@@ -50,64 +50,49 @@ function readExcel(file) {
 }
 
 async function saveQR(event, options) {
-  if(!sourcePath || !savePath) return;
+  if (!sourcePath || !savePath) return;
   const file = await readExcel(sourcePath);
   const data = file[0] && file[0].data;
-  if(!data || data.length < 0) return;
-  const prefix = savePath.split('/').pop();
+  if (!data || data.length < 0) return;
+  const prefix = savePath.split("/").pop();
   // console.log(prefix, data);
-  if (!fs.existsSync(savePath)){
+  if (!fs.existsSync(savePath)) {
     fs.mkdirSync(savePath);
   }
   let i = 0;
-  event.sender.send('process', 'all', data.length);
-  for(const row of data) {
+  event.sender.send("process", "all", data.length);
+  for (const row of data) {
+    const id = row && row[1];
+    const name = row && row[2];
     const str = row && row[0];
-    if(str && isUrl(str)) {
+    if (str && isUrl(str)) {
       i += 1;
-      const filename = `${ prefix }_${ leftPad(i, 8) }.png`;
+      const filename =
+        id && name
+          ? `${id}_${name}.png`
+          : `${prefix}_${leftPad(i, 8)}.png`;
       const filePath = path.resolve(savePath, filename);
       // console.log(row, filePath);
       await genQR(filePath, str, options);
-      event.sender.send('process', 'one', i);
-      event.sender.send('process', 'file', filename);
+      event.sender.send("process", "one", i);
+      event.sender.send("process", "file", filename);
     }
   }
   shell.showItemInFolder(savePath);
 }
 
-ipc.on('open-file-dialog', async function (event) {
-  if(isOpen) return;
+ipc.on("open-file-dialog", async function (event) {
+  if (isOpen) return;
   isOpen = true;
   try {
     const ret = await dialog.showOpenDialog({
-      properties: [ 'openFile' ],
-      filters: [
-        { name: 'Excel', extensions: [ 'xlsx', 'xls' ]},
-      ],
+      properties: ["openFile"],
+      filters: [{ name: "Excel", extensions: ["xlsx", "xls"] }],
     });
-    if(ret.filePaths && ret.filePaths.length > 0) {
+    if (ret.filePaths && ret.filePaths.length > 0) {
       const prefix = getPrefix(ret.filePaths[0]);
       sourcePath = ret.filePaths[0];
-      event.sender.send('selected-directory', ret.filePaths[0], prefix);
-    }
-  } catch (error) {
-    console.error(error);
-  } finally{
-    isOpen = false;
-  }
-});
-
-ipc.on('open-dir-dialog', async function (event) {
-  if(isOpen) return;
-  isOpen = true;
-  try {
-    const ret = await dialog.showOpenDialog({
-      properties: [ 'openDirectory' ],
-    });
-    if(ret.filePaths && ret.filePaths.length > 0) {
-      savePath = ret.filePaths[0];
-      event.sender.send('selected-dist', ret.filePaths[0]);
+      event.sender.send("selected-directory", ret.filePaths[0], prefix);
     }
   } catch (error) {
     console.error(error);
@@ -116,14 +101,32 @@ ipc.on('open-dir-dialog', async function (event) {
   }
 });
 
-ipc.on('drop-file-ok', function (event, dist) {
-  sourcePath = dist;
-  const prefix = getPrefix(sourcePath);
-  event.sender.send('selected-directory', dist, prefix);
+ipc.on("open-dir-dialog", async function (event) {
+  if (isOpen) return;
+  isOpen = true;
+  try {
+    const ret = await dialog.showOpenDialog({
+      properties: ["openDirectory"],
+    });
+    if (ret.filePaths && ret.filePaths.length > 0) {
+      savePath = ret.filePaths[0];
+      event.sender.send("selected-dist", ret.filePaths[0]);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isOpen = false;
+  }
 });
 
-ipc.on('start', async function (event, options, dist, dir) {
-  if(isOpen) return;
+ipc.on("drop-file-ok", function (event, dist) {
+  sourcePath = dist;
+  const prefix = getPrefix(sourcePath);
+  event.sender.send("selected-directory", dist, prefix);
+});
+
+ipc.on("start", async function (event, options, dist, dir) {
+  if (isOpen) return;
   isOpen = true;
   savePath = path.resolve(dist, dir);
   try {
@@ -133,5 +136,5 @@ ipc.on('start', async function (event, options, dist, dir) {
   } finally {
     isOpen = false;
   }
-  event.sender.send('gen-done');
+  event.sender.send("gen-done");
 });
